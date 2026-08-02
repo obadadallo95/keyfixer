add4f0bd6dba85fdfa47ac648756232bc8aee10e
Obada Dallo <88912887+obadadallo95@users.noreply.github.com>
2026-08-02 02:37:09 +0200

    chore(ci): trigger release artifact build on push

--- test-debug.js
+++
@@ -1,6 +0,0 @@
-const { JSDOM } = require('jsdom');
-const dom = new JSDOM('<!DOCTYPE html><p>Hello world</p>');
-const div = dom.window.document.createElement('div');
-div.contentEditable = 'true';
-dom.window.document.body.appendChild(div);
-console.log('isContentEditable:', div.isContentEditable);
